f96e7dd213bd4eb07843c6e76b9a5b898fb4af30
tanzim077 <tanzimahmed1994@gmail.com>
2024-01-14 02:45:36 +0600

    Refactor user controller to handle requests

--- src/modules/v1/user/user.controller.js
+++ src/modules/v1/user/user.controller.js
@@ -18,116 +18,87 @@ const handleCatch = require("../../../utils/handleCatch");
 class UserController {
   constructor(UserService) {
     this._userService = UserService;
-    this.getUser = this.getUser.bind(this);
-    this.deleteUser = this.deleteUser.bind(this);
-    this.getAllUsers = this.getAllUsers.bind(this);
-    this.updateUser = this.updateUser.bind(this);
-    this.createUser = this.createUser.bind(this);
-    this.changeUserRole = this.changeUserRole.bind(this);
-    this.deactivateUser = this.deactivateUser.bind(this);
-    this.deleteUserByEmail = this.deleteUserByEmail.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
+    this.getUser = this.handleRequest(this.getUser.bind(this));
+    this.deleteUser = this.handleRequest(this.deleteUser.bind(this));
+    this.getAllUsers = this.handleRequest(this.getAllUsers.bind(this));
+    this.updateUser = this.handleRequest(this.updateUser.bind(this));
+    this.createUser = this.handleRequest(this.createUser.bind(this));
+    this.changeUserRole = this.handleRequest(this.changeUserRole.bind(this));
+    this.deactivateUser = this.handleRequest(this.deactivateUser.bind(this));
+    this.deleteUserByEmail = this.handleRequest(this.deleteUserByEmail.bind(this));
+    this.logIn = this.handleRequest(this.logIn.bind(this));
+    this.logOut = this.handleRequest(this.logOut.bind(this));
+  }
+
+  handleRequest(handler) {
+    return async (req, res) => {
+      try {
+        await handler(req, res);
+      } catch (err) {
+        handleCatch(req, res, err);
+      }
+    };
   }
 
   async getUser(req, res) {
-    try {
-      const { id } = req.params;
-      const user = await this._userService.getUser(id);
-      return res.send(user);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { id } = req.params;
+    const user = await this._userService.getUser(id);
+    return res.send(user);
   }
 
   async getAllUsers(req, res) {
-    try {
-      const users = await this._userService.getAllUsers();
-      return res.send(users);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const users = await this._userService.getAllUsers();
+    return res.send(users);
   }
 
   async updateUser(req, res) {
-    try {
-      const { body } = req;
-      const { id } = req.params;
-      const updatedUser = await this._userService.updateUser(id, body);
-      return res.send(updatedUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { body } = req;
+    const { id } = req.params;
+    const updatedUser = await this._userService.updateUser(id, body);
+    return res.send(updatedUser);
   }
 
   async deleteUser(req, res) {
-    try {
-      const { id } = req.params;
-      const deletedUser = await this._userService.deleteUser(id);
-      return res.send(deletedUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { id } = req.params;
+    const deletedUser = await this._userService.deleteUser(id);
+    return res.send(deletedUser);
   }
+
   async deleteUserByEmail(req, res) {
-    try {
-      const { email } = req.params;
-      const deletedUser = await this._userService.deleteUserByEmail(email);
-      return res.status(200).send(deletedUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { email } = req.params;
+    const deletedUser = await this._userService.deleteUserByEmail(email);
+    return res.status(200).send(deletedUser);
   }
 
   async createUser(req, res) {
-    try {
-      const { body } = req;
-      const newUser = await this._userService.createUser(body);
-      return res.send(newUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { body } = req;
+    const newUser = await this._userService.createUser(body);
+    return res.send(newUser);
   }
 
   async changeUserRole(req, res) {
-    try {
-      const { body } = req;
-      const { id } = req.params;
-      const updatedUser = await this._userService.changeUserRole(id, body);
-      return res.send(updatedUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { body } = req;
+    const { id } = req.params;
+    const updatedUser = await this._userService.changeUserRole(id, body);
+    return res.send(updatedUser);
   }
 
   async deactivateUser(req, res) {
-    try {
-      const { id } = req.params;
-      const updatedUser = await this._userService.deactivateUser(id);
-      return res.send(updatedUser);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { id } = req.params;
+    const updatedUser = await this._userService.deactivateUser(id);
+    return res.send(updatedUser);
   }
 
   async logIn(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = await this._userService.logIn(email, password);
-      return res.send(user);
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { email, password } = req.body;
+    const user = await this._userService.logIn(email, password);
+    return res.send(user);
   }
 
   async logOut(req, res) {
-    try {
-      const { user, token } = req;
-      await this._userService.logOut(user, token);
-      return res.send("Logged out");
-    } catch (err) {
-      handleCatch(req, res, err);
-    }
+    const { user, token } = req;
+    await this._userService.logOut(user, token);
+    return res.send("Logged out");
   }
 }
 
